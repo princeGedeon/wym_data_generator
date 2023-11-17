a2ee803a62d222cdc99aea2560f557ba95b396fc
princegedeon <princeyisegnon@gmail.com>
2023-11-17 18:05:38 +0100

    update

--- constants/params.py
+++ constants/params.py
@@ -1,4 +1,4 @@
-TOKEN="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"
+TOKEN="VOTRE CLE API"
 # Définissez le nombre de réessais
 NESSAI = 5
 DELAI = 30  # Temps d'attente en secondes (ajustez selon vos besoins)
